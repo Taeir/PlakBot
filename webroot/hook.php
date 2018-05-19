c95907905f33b7cfbe853c076f8455dc3c815b62
Rens Hamburger <renshamburger@gmail.com>
2018-05-19 12:25:56 +0000

    Remove debug code

--- webroot/hook.php
+++ webroot/hook.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
 
 // Load composer
 require __DIR__ . '/../vendor/autoload.php';
@@ -11,14 +9,13 @@ $commands_paths = [
     __DIR__ . '/../Commands/',
 ];
 
+try {
     // Create Telegram API object
 
     $userName = $configs['username'];
 
     $telegram = new Longman\TelegramBot\Telegram($configs['api_key'], $userName);
     $telegram->addCommandsPaths($commands_paths);
-
-//var_dump($telegram->getCommandsList()); echo "ok";
 
     if ($configs['log_errors'] === true) {
         Longman\TelegramBot\TelegramLog::initErrorLog($configs['log_location'] . "/{$userName}_error.log");
@@ -33,4 +30,8 @@ $commands_paths = [
     $telegram->enableLimiter();
 
     $telegram->handle();
-
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    Longman\TelegramBot\TelegramLog::error($e);
+} catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
+    //Logging went wrong, just swallow the exception in this case.
+}
